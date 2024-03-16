4a326842517f9c9f9e1b5f13d33975499b654693
DiyanLyubchev <diyan.lyubchev@gmail.com>
2024-03-16 15:07:29 +0200

    Separate blazor from api and update adding of book

--- BookReaderAPI/Models/Response/BookDetailsResponse.cs
+++ BookReaderAPI/Models/Response/BookDetailsResponse.cs
@@ -2,6 +2,7 @@
 {
     public class BookDetailsResponse
     {
+        public int BookId { get; set; }
         public string Title { get; set; }
         public int Pages { get; set; }
         public string Author { get; set; }
--- BookReaderAPI/Service/APIService.cs
+++ BookReaderAPI/Service/APIService.cs
@@ -15,6 +15,7 @@ namespace BookReaderAPI.Service
             List<BookDetailsResponse> booksDetails = _repository.GetAll(filter: null, includeProperties: x => x.BookPicture)
                                                                 .Select(book => new BookDetailsResponse
                                                                 {
+                                                                    BookId = book.Id,
                                                                     Author = book.Author,
                                                                     Pages = book.Pages,
                                                                     Title = book.Title,
@@ -27,44 +28,36 @@ namespace BookReaderAPI.Service
 
         public string AddBookIfNotExist(byte[] bookContent)
         {
+            if (bookContent == null || bookContent.Length <= 0)
+            {
+                return $"The Book cannot be added!";
+            }
+
             string message = string.Empty;
-            //TODO: This will be removed when the client upload feature is implemented
-            string pathToBook = @"D:\The-Simple-Path-to-Wealth.pdf";
-            try
-            {
-                bookContent = File.ReadAllBytes(pathToBook);
-            }
-            catch (Exception)
-            {
-            }
+            PdfReader reader = new(bookContent);
+            string title = reader.Info["Title"];
+            string authour = reader.Info["Author"];
+            BookDetails bookDetails = _repository.GetFirstOrDefault(filter: x => x.Title == title && x.Author == authour);
 
-            if (bookContent != null && bookContent.Length > 0) 
+            if (bookDetails == null)
             {
-                PdfReader reader = new(bookContent);
-                string title = reader.Info["Title"];
-                string authour = reader.Info["Author"];
-                BookDetails bookDetails = _repository.GetFirstOrDefault(filter: x => x.Title == title && x.Author == authour);
-
-                if (bookDetails == null)
+                message = $"Book {title} from authour: {authour} was added successfully!";
+                byte[] pictureBytes = reader.GetPageContent(1);
+                bookDetails = new()
                 {
-                    message = $"Book {title} from authour: {authour} was added successfully!";
-                    byte[] pictureBytes = reader.GetPageContent(1);
-                    bookDetails = new()
-                    {
-                        Title = title,
-                        Pages = reader.NumberOfPages,
-                        Author = authour,
-                        BookPicture = new() { Picture = pictureBytes },
-                        BookContent = new() { Content = bookContent }
-                    };
+                    Title = title,
+                    Pages = reader.NumberOfPages,
+                    Author = authour,
+                    BookPicture = new() { Picture = pictureBytes },
+                    BookContent = new() { Content = bookContent }
+                };
 
-                    _repository.InsertWithSave(bookDetails);
-                }
-                else
-                    message = $"Book with title {title} and from authour: {authour} already exists!";
+                _repository.InsertWithSave(bookDetails);
             }
+            else
+                message = $"Book with title {title} and from authour: {authour} already exists!";
 
-            return string.IsNullOrEmpty(message) ? "The Book cannot be added!" : message;
+            return message;
         }
 
         public byte[] GetBookContentById(int id)
